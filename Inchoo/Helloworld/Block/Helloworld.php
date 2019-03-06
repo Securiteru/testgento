d7057c24126d581ae067ca2403c218c69eb54e53
Adrian Padurean <adrian.padurean@sepa.at>
2019-03-06 06:45:03 +0200

    working shit

--- Inchoo/Helloworld/Block/Helloworld.php
+++ Inchoo/Helloworld/Block/Helloworld.php
@@ -1,12 +1,16 @@
 <?php
 namespace Inchoo\Helloworld\Block;
 
+use Magento\Eav\Model\ResourceModel\Entity\Attribute\OptionFactory;
+
 class Helloworld extends \Magento\Framework\View\Element\Template
 {
     protected $_productRepository;
     protected $_productCollectionFactory;
     protected $_stockRegistry;
     protected $_attributeSet;
+    protected $_optionFactory;
+    protected $_options;
 
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
@@ -14,12 +18,15 @@ class Helloworld extends \Magento\Framework\View\Element\Template
         \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
         \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry,
         \Magento\Eav\Api\AttributeSetRepositoryInterface $attributeSet,
+        OptionFactory $optionFactory,
+
         array $data = []
     ) {
         $this->_productRepository = $productRepository;
         $this->_productCollectionFactory = $productCollectionFactory;
         $this->_stockRegistry=$stockRegistry;
         $this->_attributeSet = $attributeSet;
+        $this->_optionFactory = $optionFactory;
         parent::__construct($context, $data);
     }
 
@@ -32,7 +39,7 @@ class Helloworld extends \Magento\Framework\View\Element\Template
     {
         $collection = $this->_productCollectionFactory->create();
         $collection->addAttributeToSelect('*');
-        $collection->setPageSize(5);
+        $collection->setPageSize(0);
         return $collection;
     }
 
@@ -57,5 +64,31 @@ class Helloworld extends \Magento\Framework\View\Element\Template
     {
         $attributeSetRepository = $this->_attributeSet->get($product->getAttributeSetId());
         return $attributeSetRepository->getAttributeSetName();
+    }
+
+    public function getAllOptions()
+    {
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+        $coll = $objectManager->create(\Magento\Catalog\Model\Product\AttributeSet\Options::class);
+
+        $this->_options=[ ['label'=>'Select Attribute Set', 'value'=>'']];
+
+        foreach ($coll->toOptionArray() as $d) {
+            if ($d['label'] !== 'Default') {
+                $this->_options[] = ['label' => $d['label'], 'value' => $d['value']];
+            }
+        }
+        return $this->_options;
+    }
+
+    public function getOptionText($value)
+    {
+        foreach ($this->getAllOptions() as $option) {
+            if ($option['value'] == $value) {
+                return $option['label'];
+            }
+        }
+        return false;
     }
 }
